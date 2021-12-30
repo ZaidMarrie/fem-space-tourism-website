1fc986b7e10c7d3562abcb71d1c3bef0b413d689
ZaidMarrie <zaidmarrie27@gmail.com>
2021-12-30 18:17:04 +0200

    add styling & functionality to navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-fetch('../../data.json')
-    .then(res => res.json())
-    .then(data => console.log(data))
+const menuToggleBtn = document.querySelector('.nav-toggle')
+const navMenu = document.getElementById('navigation')
+
+// Toggle navMenu open/close
+function toggleMenu(event) {
+    const visibility = navMenu.getAttribute('data-visible')
+
+    if(visibility === 'false') {
+        navMenu.setAttribute('data-visible', true)
+        menuToggleBtn.setAttribute('aria-expanded', true)
+    } else {
+        navMenu.setAttribute('data-visible', false)
+        menuToggleBtn.setAttribute('aria-expanded', false)
+    }
+}
+
+menuToggleBtn.addEventListener('click', toggleMenu)
