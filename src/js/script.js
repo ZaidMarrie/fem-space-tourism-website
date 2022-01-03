df74e67006ed729c5537d334aad4fa982f8504a4
ZaidMarrie <zaidmarrie27@gmail.com>
2022-01-03 12:14:36 +0200

    add markup & styling to remaining pages

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,36 @@ function toggleMenu(event) {
 }
 
 menuToggleBtn.addEventListener('click', toggleMenu)
+
+// Tabs
+const tablist = document.querySelector('[role="tablist"]')
+const tabs = document.querySelectorAll('[role="tab"]')
+let tabPosition = 0
+
+function changeTabFocus(e) {
+    const leftKey = 37
+    const rightKey = 39
+
+    if (e.keyCode === leftKey || e.keyCode === rightKey) {
+        tabs[tabPosition].setAttribute('tabindex', -1)
+    } 
+
+    if (e.keyCode === rightKey) {
+        tabPosition++
+        if (tabPosition >= tabs.length) {
+            tabPosition = 0
+        }
+    }
+
+    if (e.keyCode === leftKey) {
+        tabPosition--
+        if (tabPosition < 0) {
+            tabPosition = tabs.length - 1
+        }
+    }
+    console.log(tabPosition)
+    tabs[tabPosition].setAttribute('tabindex', 0)
+    tabs[tabPosition].focus()
+}
+
+tablist.addEventListener('keydown', changeTabFocus)
